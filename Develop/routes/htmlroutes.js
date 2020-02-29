608a0ee6a4b0d83fa12ca28bb433d09bb153c9d8
Ben Calvert <benhcalvert@gmail.com>
2020-02-29 08:39:44 -0700

    updated server.js

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -2,15 +2,17 @@
 const path = require("path");
 const router = require("express").Router()
 
-// HTML Routes
-router.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+module.exports = function(app) {
 
-// default route is home
-router.get("/", function(req, res) {
-res.sendFile(path.join(__dirname, "../public/index.html"));
+    // HTML Routes
+    router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+    
+    // default route is home
+    router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+    
+    })
 
-})
-
-module.exports = router;
+}
--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,12 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-var fs = require('fs');
-var htmlRoutes = require("./routes/htmlRoutes")
-var apiRoutes = require("./routes/apiRoutes")
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 2006;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 
 // Sets up the Express app to handle data parsing
@@ -17,11 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use("/api", apiRoutes)
-app.use("/", htmlRoutes)
-
-
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
