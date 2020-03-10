cde08b33058c2c264205653a833dcb4a43608f2b
Ben Calvert <benhcalvert@gmail.com>
2020-03-09 20:17:23 -0600

    all routes working. needed IDS

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,55 +1,54 @@
 const db = require("../db/db.json");
 const fs = require("fs");
-
+let noteDB = JSON.parse(fs.readFileSync("./db/db.json"));
+let ids = noteDB.map(note => note.id);
+let noteId = 0;
 
 // display notes from db to the page
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
-    res.send(db);
+    res.send(noteDB);
   });
 
-
 //   If POST request, create new note
-  app.post("/api/notes", function(req, res) {
+  app.post("/api/notes", function(req, res) {      
 
-    let noteId = ''
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text
-    };
+      let flag = true;
+      while (flag) {
+        if (!ids.includes(noteId)) {
+          flag = false;
+          ids.push(noteId);
+        } else {
+          noteId ++;
+        };
+      };
+      
+      let newNote = {
+        id: noteId,
+        title: req.body.title,
+        text: req.body.text
+      };
 
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
+      noteDB.push(newNote);
 
-      const allNotes = JSON.parse(data);
-
-      allNotes.push(newNote);
-
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+      fs.writeFile("./db/db.json", JSON.stringify(noteDB, null, 2), err => {
         if (err) throw err;
         res.send(db);
-        console.log("Note added")
+        console.log("note added")
       });
-    });
   });
 
   //   If DELETE request, delete note with id of req.params.id
   app.delete("/api/notes/:id", (req, res) => {
 
-    let noteId = req.params.id;
+    let identifier = req.params.id;
 
-    fs.readFile("../db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
+      noteDB = noteDB.filter(note => note.id != identifier);
 
-      const allNotes = JSON.parse(data);
-      const newAllNotes = allNotes.filter(note => note.id != noteId);
-
-      fs.writeFile("../db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
+      fs.writeFile("./db/db.json", JSON.stringify(noteDB, null, 2), err => {
         if (err) throw err;
-        res.send(db);
-        console.log("note deleted.")
+        res.send(noteDB);
+        console.log("note deleted")
       });
-    });
   });
 };
